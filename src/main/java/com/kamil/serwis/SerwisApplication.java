c898746f1094efa8c8b95e5bc67f26597d851bc6
kamilszwaj@gmail.com <kamilszwaj@gmail.com>
2019-10-08 22:37:25 +0200

    First commit, template

---
+++ src/main/java/com/kamil/serwis/SerwisApplication.java
@@ -0,0 +1,13 @@
+package com.kamil.serwis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SerwisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SerwisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kamil/serwis/SerwisApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kamil.serwis;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SerwisApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
